abe3be5cc454d0ff59fb38bd519115a4affe6ea9
unknown <agrawal.shruti998@gmail.com>
2016-12-27 17:15:50 +0530

    First commit

---
+++ JScript.js
@@ -0,0 +1,16 @@
+﻿function get_details(){
+var temp={ 
+           name:"",
+           age:0,
+           place:"",
+           date:""
+         };
+temp.name=document.getElementById("name").value;
+temp.age=document.getElementById("age").value;
+temp.date=document.getElementById("dob").value;
+temp.place=document.getElementById("placeOfBirth").value;
+console.log(temp.name);
+console.log(temp.age);
+console.log(temp.date);
+console.log(temp.place);
+}
